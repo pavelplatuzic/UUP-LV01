1f643f5a68af871980c782556468ee39a34fc292
pavelplatuzic <pavel.platuzic@skole.hr>
2025-12-18 22:06:35 +0100

    Gotov program.
    Added fuel consumption and cost calculations to the program.

--- LV01/LV01.cpp
+++ LV01/LV01.cpp
@@ -1,20 +1,28 @@
-// LV01.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <iomanip>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    double prijedeniKm, potrosnjaVozila, cijenaGoriva;
+
+    cout << "Unesite broj prijedenih kilometara: ";
+    cin >> prijedeniKm;
+
+    cout << "Unesite potrosnju vozila (litara na 100 km): ";
+    cin >> potrosnjaVozila;
+
+    cout << "Unesitte cijenu goriva po litri (u eurima): ";
+    cin >> cijenaGoriva;
+
+    double potrosenoGoriva = (prijedeniKm * potrosnjaVozila) / 100;
+    double ukupniTrosakGoriva = potrosenoGoriva * cijenaGoriva;
+
+    int sladoled =67
+
+    cout << setprecision(4);
+    cout << "Potroseno goriva: " << potrosenoGoriva << " litara\n";
+    cout << "Ukupni trosak goriva za putovanje: " << ukupniTrosakGoriva << " eura\n";
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
